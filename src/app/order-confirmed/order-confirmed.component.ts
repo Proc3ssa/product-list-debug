8d157bb0daa8fe715995aec7e5647a17faac54ae
Faisal <pros3sa@gmail.com>
2025-06-12 08:32:42 +0000

    fix: set subscribed data to order confirmed

--- src/app/order-confirmed/order-confirmed.component.ts
+++ src/app/order-confirmed/order-confirmed.component.ts
@@ -1,14 +1,19 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { Dessert } from '../interfaces/main';
 
 @Component({
   selector: 'app-order-confirmed',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, CurrencyPipe],
   templateUrl: './order-confirmed.component.html',
   styleUrl: './order-confirmed.component.scss'
 })
 export class OrderConfirmedComponent {
   
   @Input() isOrderConfirmedVisible: boolean = false;
+  @Input() cartItems$: Observable<Dessert[]> | undefined;
   @Output() closeModal = new EventEmitter<void>();
  
   onClose() {
