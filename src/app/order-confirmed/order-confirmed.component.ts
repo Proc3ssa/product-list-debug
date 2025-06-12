45f0b38bf69249df44e6fe95ced55d4fe80aa599
Faisal <pros3sa@gmail.com>
2025-06-12 08:50:27 +0000

    feat: add clear cart feature

--- src/app/order-confirmed/order-confirmed.component.ts
+++ src/app/order-confirmed/order-confirmed.component.ts
@@ -3,6 +3,7 @@ import { CommonModule, CurrencyPipe } from '@angular/common';
 import { Observable } from 'rxjs';
 import { Dessert } from '../interfaces/main';
 import { GetTotalPipe } from '../pipes/get-total.pipe';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-order-confirmed',
@@ -12,12 +13,15 @@ import { GetTotalPipe } from '../pipes/get-total.pipe';
   styleUrl: './order-confirmed.component.scss'
 })
 export class OrderConfirmedComponent {
+
+  constructor(private cartService: CartService) {}
   
   @Input() isOrderConfirmedVisible: boolean = false;
   @Input() cartItems$: Observable<Dessert[]> | undefined;
   @Output() closeModal = new EventEmitter<void>();
  
   onClose() {
+    this.cartService.clearCart();
     this.closeModal.emit();
   }
  }
